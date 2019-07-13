a59ac6bc17b87d14c523606a977902fbfe059fdb
ardi <arizmaldi@gmail.com>
2019-07-13 15:14:55 +0700

    Style update in fraction component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FractionComponent } from './fraction/fraction.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' }
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', component: FractionComponent}
 ];
 
 @NgModule({
--- src/app/fraction/fraction-routing.module.ts
+++ src/app/fraction/fraction-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FractionComponent } from './fraction.component';
 
 const routes: Routes = [
-    { path: 'welcome', component: FractionComponent},
 ];
 
 @NgModule({
