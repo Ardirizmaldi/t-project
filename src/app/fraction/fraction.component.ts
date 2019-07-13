ced14cbac5495297e55642538a0dc2a45fb0f2c8
ardi <arizmaldi@gmail.com>
2019-07-13 17:11:15 +0700

    make it better

--- src/app/fraction/fraction.component.ts
+++ src/app/fraction/fraction.component.ts
@@ -9,6 +9,7 @@ import { FormControl, FormArray } from '@angular/forms';
 
 export class FractionComponent implements OnInit {
   money: any;
+  change: any;
   fraction = [100000, 50000, 20000, 10000, 5000, 1000, 500, 100, 50];
   fcount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
@@ -19,7 +20,7 @@ export class FractionComponent implements OnInit {
   }
 
   // function money fraction
-  onSubmit() {
+  onSubmit(output: string) {
     let i = 0; let creminder = this.money; let ccoin;
     while (i < this.fraction.length) {
       while (this.fraction[i] <= creminder) {
@@ -27,20 +28,20 @@ export class FractionComponent implements OnInit {
         ccoin = this.fcount[i];
         ccoin += 1;
         this.fcount[i] = ccoin;
-
       }
-
       i++;
     }
-
+    this.change = creminder;
     console.log(this.fcount);
     console.log(this.money);
+    console.log(this.change);
   }
 
   // function clear input
   onClear() {
     this.money = null;
     this.fcount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.change = null;
   }
 
 }
